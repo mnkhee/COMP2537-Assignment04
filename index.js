b3f6f73923b4149f686ec4db65efaaba0dd6f128
Joshua Chuah <joshua2004cc@gmail.com>
2023-05-18 23:32:49 -0700

    bug fixes

--- index.js
+++ index.js
@@ -33,20 +33,84 @@ const stopTimer = () => {
 };
 
 const setup = async () => {
-    let response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
-    let gridHTML = '';
-    pokemonList = response.data.results;
-    
-    // Shuffles the pokemon images so that different pokemon appear each time.
-    shuffleImage(pokemonList);
-    
-    $('#total').html(EASY);
+  let response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+  let gridHTML = '';
+  let difficulty;
 
-    for (let i = 0; i < EASY; i++) {
-      const pokemon = pokemonList[i];
+  pokemonList = response.data.results;
+
+  // Shuffles the pokemon images so that different pokemon appear each time.
+  shuffleImage(pokemonList);
+
+  $('#option1').on('click', function () {
+    gridHTML = '';
+    stopTimer();
+    seconds = 0;
+    $('#time').html(seconds);
+    $('#game_grid').css('display', 'none');
+    $('#game_grid').css('width', '600px');
+    $('#game_grid').css('height', '400px');
+    difficulty = 'EASY';
+    updateTotal(difficulty); // Call a separate function to update the total
+  });
+
+  $('#option2').on('click', function () {
+    gridHTML = '';
+    stopTimer();
+    seconds = 0;
+    $('#time').html(seconds);
+    $('#game_grid').css('display', 'none');
+    $('#game_grid').css('width', '800');
+    $('#game_grid').css('height', '600px');
+    difficulty = 'MEDIUM';
+    updateTotal(difficulty); // Call a separate function to update the total
+  });
+
+  $('#option3').on('click', function () {
+    gridHTML = '';
+    stopTimer();
+    seconds = 0;
+    $('#time').html(seconds);
+    $('#game_grid').css('display', 'none');
+    $('#game_grid').css('width', '1200px');
+    $('#game_grid').css('height', '800px');
+    difficulty = 'HARD';
+    updateTotal(difficulty); // Call a separate function to update the total
+  });
+
+  $('#start').on('click', function () {
+    $('#game_grid').css('display', 'flex');
+    startTimer();
+  });
+
+  $('#reset').on('click', function () {
+    stopTimer();
+    seconds = 0;
+    $('#time').html(seconds);
+  });
+
+  $('#dark').on('click', function () {
+    $('#game_grid').css('background-color', 'black');
+  });
+
+  $('#light').on('click', function () {
+    $('#game_grid').css('background-color', 'white');
+  });
+
+  function updateTotal(difficulty) {
+    const difficultyValue = getDifficultyCount(difficulty);
+    $('#total').html(difficultyValue);
+    generateGrid(difficulty);
+  }
+
+  async function generateGrid(difficulty) {
+    const selectedPokemonList = pokemonList.slice(0, getDifficultyCount(difficulty));
+
+    for (let i = 0; i < selectedPokemonList.length; i++) {
+      const pokemon = selectedPokemonList[i];
       const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
       const pokemonImage = res.data.sprites.other['official-artwork'].front_default;
-      
+
       gridHTML += `
         <div style="width: 200px">
           <img src="${pokemonImage}" alt="${pokemon.name}"/>
@@ -56,54 +120,34 @@ const setup = async () => {
         </div>
       `;
     }
-    
+
     $('#game_grid').html(gridHTML);
+  }
 
-    $('#option1').on('click', function () {
-        $('#game_grid').css('width', '600px');
-        $('#game_grid').css('height', '400px');
-    });
+  function getDifficultyCount(difficulty) {
+    switch (difficulty) {
+      case 'EASY':
+        return 3;
+      case 'MEDIUM':
+        return 6;
+      case 'HARD':
+        return 12;
+      default:
+        return 3;
+    }
+  }
+};
 
-    $('#option2').on('click', function () {
-        $('#game_grid').css('width', '800');
-        $('#game_grid').css('height', '600px');
-    });
-    
-    $('#option3').on('click', function () {
-        $('#game_grid').css('width', '1200px');
-        $('#game_grid').css('height', '800px');
-    });
+$(document).ready(setup);
 
-    $('#start').on('click', function () {
-        $('#game_grid').css('display', 'flex');
-        startTimer();
-      });
 
-    $('#reset').on('click', function () {
-        stopTimer();
-        seconds = 0;
-        $('#time').html(seconds);
-      });
-
-    $('#dark').on('click', function () {
-        $('#game_grid').css('background-color', 'black');
-      });
-    
-      $('#light').on('click', function () {
-        $('#game_grid').css('background-color', 'white');
-      });
-    };
-  
-  $(document).ready(setup);
-  
-
-  /**
-   * TODO: 
-   * - Add a tracker for how many clicks it took to finish the game
-   * - Add a tracker for how many pairs were found / how many remain
-   * - Check if user has clicked on easy, medium, or hard, then load the appropriate number of pokemon.
-   *    - Change the size of the game grid according the the difficulty
-   * - Add a button to start the game
-   * - Add a button to reset the game
-   * - Randomize placement of pokemon images
-   */
+/**
+ * TODO: 
+ * - Add a tracker for how many clicks it took to finish the game
+ * - Add a tracker for how many pairs were found / how many remain
+ * - Check if user has clicked on easy, medium, or hard, then load the appropriate number of pokemon.
+ *    - Change the size of the game grid according the the difficulty
+ * - Add a button to start the game
+ * - Add a button to reset the game
+ * - Randomize placement of pokemon images
+ */
